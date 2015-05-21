356e7665c399f322a42d8c03b6ffaece388d377b
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-05-21 17:26:45 -0400

    Add max_depth

--- zones.c
+++ zones.c
@@ -12,8 +12,16 @@
 #define MIN_COVERAGE 0.0199999
 #define MAX_COVERAGE 0.4000001
 
+/* default seed */
+#define DEFAULT_SEED 42
+
+/* depth: bounds and coverage */
+#define MIN_DEPTH 0.0999999
+#define MAX_DEPTH 1
+#define DEFAULT_DEPTH 1
+
 /* options as string */
-#define OPTSTR "R:s:c:"
+#define OPTSTR "R:s:c:d:"
 
 /* Command line arguments */
 static struct {
@@ -21,6 +29,8 @@ static struct {
 	int sz;
 	/* coverage */
 	float coverage;
+	/* max depth */
+	float depth;
 
 	/* random seed */
 	long int seed;
@@ -33,6 +43,7 @@ static void usage(const char *prg)
 	fprintf(stderr, "OPTIONS:\n");
 	fprintf(stderr, "\t-s sz\tlog(size) of the length of the space (int)\n");
 	fprintf(stderr, "\t-c coverage\tratio of alert zone's area to the area of the space (real)\n");
+	fprintf(stderr, "\t-d depth\tmax expansion depth in percents of the max depth(real, default 1.0)\n");
 	fprintf(stderr, "\t-R seed\tseed of the random number generator, (int, default 42)\n");
 	fprintf(stderr, "\n");
 	fprintf(stderr, "CONSTRAINTS:\n");
@@ -46,6 +57,7 @@ static void print_args()
 	printf("Grid side length: %d\n", args.sz);
 	printf("Coverage: %f\n", args.coverage);
 	printf("Seed: %ld\n", args.seed);
+	printf("Depth: %f\n", args.depth);
 }
 
 static void parse_arguments(int argc, char **argv)
@@ -59,6 +71,9 @@ static void parse_arguments(int argc, char **argv)
 		printf("%s ", argv[i]);
 	printf("\n");
 
+	args.seed = DEFAULT_SEED;
+	args.depth = DEFAULT_DEPTH;
+
 	while((opt = getopt(argc, argv, OPTSTR)) != -1)
 		switch(opt) {
 		case 's':
@@ -67,6 +82,10 @@ static void parse_arguments(int argc, char **argv)
 			break;
 		case 'c':
 			if (sscanf(optarg, "%f%c", &args.coverage, &extra) != 1)
+				usage(argv[0]);
+			break;
+		case 'd':
+			if (sscanf(optarg, "%f%c", &args.depth, &extra) != 1)
 				usage(argv[0]);
 			break;
 		case 'R':
@@ -79,13 +98,20 @@ static void parse_arguments(int argc, char **argv)
 	/* consistency checks */
 	if (optind != argc)
 		usage(argv[0]); /* extra arguments */
+
 	if (MIN_SZ > args.sz || args.sz > MAX_SZ) {
 		fprintf(stderr, "Invalid sz argument");
 		usage(argv[0]);
 	} else
 		args.sz = 1 << args.sz;
+
 	if (MIN_COVERAGE > args.coverage || args.coverage > MAX_COVERAGE) {
 		fprintf(stderr, "Invalid coverage argument");
+		usage(argv[0]);
+	}
+
+	if (MIN_DEPTH > args.depth || args.depth > MAX_DEPTH) {
+		fprintf(stderr, "Invalid depth argument");
 		usage(argv[0]);
 	}
 }
@@ -96,6 +122,7 @@ int main(int argc, char **argv)
 
 	parse_arguments(argc, argv);
 	print_args();
+	return 0;
 
 	space = space_gen(args.sz);
 	space_generate_zone(space, args.coverage, args.seed);
