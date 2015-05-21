2d5fa17d5f3013396e2a44ac405f5a79d11b3a0d
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-05-21 17:51:12 -0400

    Run script

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sizes="5 6 7 8 9"
+depths="1.0 0.5 0.25 0.1"
+coverages="0.02 0.04 0.06 0.08 0.1"
+seeds="24125162166 12512616216 254125521"
+
+espresso="../orig/espresso/espresso -sx -"
+
+for seed in ${seeds}; do
+    for size in ${sizes}; do
+        for depth in ${depths}; do
+            for coverage in ${coverages}; do
+                ./zones -s ${size} -c ${coverage} -d ${depth} -R ${seed}    | ${espresso}
+                ./zones -s ${size} -c ${coverage} -d ${depth} -R ${seed} -f | ${espresso}
+            done
+        done
+    done
+done
